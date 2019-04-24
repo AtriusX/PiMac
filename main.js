9b9b8cead5aa7658da0111d0eb1de54ce68a66df
Atrius <atriusedm@gmail.com>
2019-04-24 18:11:59 -0400

    Edge detection manipulation
    Pie menu changes structure when near edges of a display

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ const path 	   = require('path');
 const ioHook   = require('iohook');
 const { app, BrowserWindow, ipcMain } = electron
 
-let mainWindow;
+let wheelWindow;
 
 app.on('ready', function() {
 	const { screen } = electron
-	mainWindow = new BrowserWindow({
+	wheelWindow = new BrowserWindow({
 		transparent: true, frame: false, skipTaskbar: true, alwaysOnTop: true
 	});
 
-	mainWindow.loadURL(url.format({
+	wheelWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'main.html'),
 		protocol: 'file', slashes: true
 	}));
 
-	mainWindow.webContents.on('dom-ready', () => {
+	wheelWindow.webContents.on('dom-ready', () => {
 		var active = false;
 		// Show window when key is pressed
 		ioHook.on('keydown', (event) => {
@@ -25,16 +25,16 @@ app.on('ready', function() {
 			if (event.keycode == 15 && !active) {
 				var disp = screen.getDisplayNearestPoint(screen.getCursorScreenPoint());
 				// Move window to active screen
-				mainWindow.setPosition(disp.workArea.x, disp.workArea.y);
-				mainWindow.maximize();
+				wheelWindow.setPosition(disp.workArea.x, disp.workArea.y);
+				wheelWindow.maximize();
 				// Send the wheel data to the UI
-				mainWindow.webContents.send(
+				wheelWindow.webContents.send(
 					"window:show", 
 					["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"], 
 					screen.getCursorScreenPoint(), 
 					disp
 				);
-				mainWindow.setIgnoreMouseEvents(false);
+				wheelWindow.setIgnoreMouseEvents(false);
 				// Keep the wheel from refreshing when the key is held down
 				active = true;
 			}
@@ -42,8 +42,8 @@ app.on('ready', function() {
 		// Hide the screen after the key is released
 		ioHook.on('keyup', (event) => {
 			if (event.keycode == 15) {	
-				mainWindow.webContents.send("window:hide");
-				mainWindow.setIgnoreMouseEvents(true, false);
+				wheelWindow.webContents.send("window:hide");
+				wheelWindow.setIgnoreMouseEvents(true, false);
 				// Deactivate the window
 				active = false;
 			}
@@ -52,19 +52,17 @@ app.on('ready', function() {
 		ioHook.start();
 		// Triggered when a window is hidden from the UI side
 		ipcMain.on('window:hidden', () => {
-			mainWindow.setIgnoreMouseEvents(true, false);
+			wheelWindow.setIgnoreMouseEvents(true, false);
 			visible = false
 		});
 	});
 
-	mainWindow.maximize();
-	mainWindow.show();
-	// mainWindow.webContents.openDevTools();
+	wheelWindow.maximize();
+	wheelWindow.show();
+	// wheelWindow.webContents.openDevTools();
 });
 
 // TODO Detecting the last hovered element and executing the command associated to it
-// TODO Multi-monitor support
-// TODO Edge detection (prevent the wheel from going outside the window by changing the wheel style)
 // TODO Program detection and keybind/macro config
 // TODO Settings window
 // TODO show application specific pie menu with macros
--- wheel.js
+++ wheel.js
@@ -1,28 +1,77 @@
 const nav = require('wheelnav')
 const Raphael = require('raphael')
 
-function loadMenu(options, point, disp) {
+const EDGE_BUFFER = 150;
+
+function loadMenu(options, point, display) {
 	const { ipcRenderer } = require('electron');
 	var piemenu = new wheelnav('piemenu');
-	// piemenu.sliceInitPathFunction = piemenu.slicePathFunction;
-	piemenu.initPercent = 0.1;
-	piemenu.wheelRadius = piemenu.wheelRadius * 0.83;
+	piemenu.initPercent = 1;
+	piemenu.wheelRadius = piemenu.wheelRadius * 0.93;
+	piemenu.wheelNavAngle = 90;
+	piemenu.animatetime = 0;
+	
+	var angle = sliceAngle(point, display, options.length);
+	console.log(angle);
+	if (angle != 0) {
+		piemenu.navItemsContinuous = true;
+		piemenu.sliceAngle = angle;
+		piemenu.titleRotateAngle = 0;
+		piemenu.navAngle = angle / 2 + rotation(point, display, options.length);
+	}
+
+	// piemenu.navAngle = rotation(point, display, options.length);
 	piemenu.createWheel(options);
-	for (i = 0; i < piemenu.navItems.length; i++) {
-		piemenu.navItems[i].navigateFunction = () => {
-			unloadMenu(piemenu);
+	piemenu.navItems.forEach(n => {
+		n.navigateFunction = () => {
+			unloadMenu(piemenu);	
 			ipcRenderer.send('window:hidden');
 		}
-	}
+	});
+
 	var {x, y} = point
 	var d = document.getElementById('piemenu');
 	d.style.position = "absolute";
-	
-	d.style.top = (y < 0 ? y + disp.bounds.height : y) - disp.bounds.y - 200 + "px";
-	d.style.left = x - disp.bounds.x - 200 + "px";							
+	// Some funky display math, makes sure the wheel is always centered properly on the cursor
+	d.style.top = (y < 0 ? y + display.bounds.height : y) - display.bounds.y - 200 + "px";
+	d.style.left = x - display.bounds.x - 200 + "px";							
 };
 
 function unloadMenu() {
 	// It's stupid that this works without error
 	document.getElementById("piemenu").innerHTML = "";
 }
+
+function sliceAngle(point, display, itemCount) {
+	var { x, y } = point;
+	var { height, width } = display.bounds;
+	var angle = 0, atEdge;
+	// Check if the cursor is near the left or right edge
+	if (x < display.bounds.x + EDGE_BUFFER || x > display.bounds.x + width - EDGE_BUFFER) {
+		angle = 180 / itemCount;
+		atEdge = true;
+	}
+	// Calculate top/bottom edge/corner values
+	if (y < display.bounds.y + EDGE_BUFFER || y > display.bounds.y + height - EDGE_BUFFER) angle = (atEdge ? 90 : 180) / itemCount;
+
+	return angle;
+}
+
+function rotation(point, display) {
+	var { x, y } = point;
+	var { height, width } = display.bounds;
+	var boundX = display.bounds.x;
+	var boundY = display.bounds.y;
+	var { atLeft, atRight, atTop, atBottom } = false;
+
+
+	if (x < boundX + EDGE_BUFFER) 		   atLeft = true;
+	if (x > boundX + width - EDGE_BUFFER)  atRight = true;
+	if (y < boundY + EDGE_BUFFER) 		   atTop = true;
+	if (y > boundY + height - EDGE_BUFFER) atBottom = true;
+
+	if (atTop)    return atRight ? 90 : 0;
+	if (atLeft)   return 270;
+	if (atBottom) return 180;
+	if (atRight)  return 90;
+}	
