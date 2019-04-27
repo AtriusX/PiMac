18383aa52dfb5875ea7fdd2fc260d0754d257acc
Atrius <atriusedm@gmail.com>
2019-04-27 01:41:20 -0400

    Move backend files to system directory

--- src/system/process.js
+++ src/system/process.js
@@ -1,13 +1,15 @@
 const activeWin      = require('active-win');
 const processWindows = require('node-process-windows');
 
+// TODO Mac support later?
+
 var pid = 0;
 
-exports.setProgram = function() {
+exports.set = function() {
     pid = activeWin.sync().owner.processId; 
 }   
 
-exports.setTop = function() {
+exports.focus = function() {
     if (pid == process.pid) return;
     processWindows.focusWindow(pid);
 }
